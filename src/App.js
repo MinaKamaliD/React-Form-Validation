27934aa275f9584681898ab6beed2a998206dc63
Mina <minakamali.d@gmail.com>
2023-01-24 10:15:27 +0330

    footer added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Form from "./components/Form/Form";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
     <div>
       <Form></Form>
+      <Footer></Footer>
     </div>
   );
 };
